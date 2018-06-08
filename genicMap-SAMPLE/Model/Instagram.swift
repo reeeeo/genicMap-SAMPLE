5aa3f4c5b6d94fa3178735b24060b4eb94278ba3
reeeeo <reo.117.pri@gmail.com>
2018-06-08 13:32:47 +0800

    add instagram struct

--- genicMap-SAMPLE/Model/Instagram.swift
+++ genicMap-SAMPLE/Model/Instagram.swift
@@ -19,3 +19,19 @@ struct InstagramData: Codable {
   }
 }
 
+struct Instagram {
+  static func fetchInstagramData(completion: @escaping (InstagramData) -> Swift.Void ) {
+    guard let path = Bundle.main.path(forResource: "instagram", ofType: ".json") else {
+      fatalError("Not Found : json file.")
+    }
+    guard let data = FileHandle(forReadingAtPath: path)?.readDataToEndOfFile() else  {
+      fatalError("Could Not Read : json file.")
+    }
+    do {
+      let instagram = try JSONDecoder().decode(InstagramData.self, from: data)
+      completion(instagram)
+    } catch {
+      fatalError(error.localizedDescription)
+    }
+  }
+}
